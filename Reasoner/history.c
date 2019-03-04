7c78cfba38532ae18eaf29d05971c3708da4cec8
Tanel Tammet <tanel.tammet@gmail.com>
2019-03-04 15:02:17 +0200

    fix

--- Reasoner/history.c
+++ Reasoner/history.c
@@ -1,28 +1,25 @@
 /*
-* $Id:  $
-* $Version: $
 *
-* Copyright (c) Tanel Tammet 2004,2005,2006,2007,2008,2009,2010
+* Copyright (c) Tanel Tammet 2004-2019
 *
 * Contact: tanel.tammet@gmail.com                 
 *
-* This file is part of WhiteDB
+* This file is part of GKC
 *
-* WhiteDB is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
+* GKC is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
 * 
-* WhiteDB is distributed in the hope that it will be useful,
+* GKC is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
 * 
-* You should have received a copy of the GNU General Public License
-* along with WhiteDB.  If not, see <http://www.gnu.org/licenses/>.
+* You should have received a copy of the GNU Affero General Public License
+* along with GKC.  If not, see <http://www.gnu.org/licenses/>.
 *
 */
-
  /** @file clterm.c
  *  Procedures for managing clause history.
  *
