613e357f2084c5eb369e01ab2c16bac68ac35beb
Tanel Tammet <tanel.tammet@gmail.com>
2019-03-19 21:54:57 +0200

    add lacking new files

---
+++ Reasoner/init.c
@@ -0,0 +1,74 @@
+/*
+*
+* Copyright (c) Tanel Tammet 2019
+*
+* Contact: tanel.tammet@gmail.com                 
+*
+* This file is part of GKC
+*
+* GKC is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* GKC is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU Affero General Public License
+* along with GKC.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+ /** @file init.c
+ * Initialization of reasoning-specific data structures before search
+ */
+
+/* ====== Includes =============== */
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "rincludes.h"  
+ 
+
+/* ====== Private defs =========== */
+
+
+//#define DEBUG
+#undef DEBUG  
+
+
+/* ====== Private headers ======== */
+
+  
+  
+/* ====== Functions ============== */
+
+
+/* =====================================================
+
+ 
+
+====================================================== */
+
+int init_shared_database(void* db) {
+
+  printf("\ninit_shared_database starts\n");
+
+  printf("\ninit_shared_database ends\n");
+
+  return 0;
+
+}
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
---
+++ Reasoner/init.h
@@ -0,0 +1,47 @@
+/*
+*
+* Copyright (c) Tanel Tammet 2004-2019
+*
+* Contact: tanel.tammet@gmail.com                 
+*
+* This file is part of GKC
+*
+* GKC is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* GKC is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU Affero General Public License
+* along with GKC.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+ /** @file init.h
+ * Initialization of reasoning-specific data structures before search
+ */
+
+
+#ifndef DEFINED_INIT_H
+#define DEFINED_INIT_H
+
+/* ==== Includes ==== */
+
+#include "types.h"
+#include "glb.h"
+#include "clterm.h"
+
+/* ==== Global defines ==== */
+
+
+
+/* ==== Protos ==== */
+
+int init_shared_database(void* db);
+
+
+#endif
